b8b5dfbcdaf4248c7dcd9fe2822c27bfd995a4e4
ReinhardtJ <ersatz.jonas@yahoo.de>
2020-11-13 23:11:31 +0100

    Added AIPath override class

---
+++ Assets/Scripts/EnemyAIPath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class EnemyAIPath : AIPath
+{
+    protected override void MovementUpdateInternal(float deltaTime,
+                                                   out Vector3 nextPosition,
+                                                   out Quaternion nextRotation)
+    {
+        // TODO(Jonas): Override so that movement is based on accelleration, not 
+        // teleportation
+         
+        // base.MovementUpdateInternal(deltaTime, out nextPosition, out nextRotation);
+    
+    }
+}
