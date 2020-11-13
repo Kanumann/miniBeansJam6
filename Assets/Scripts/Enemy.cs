f57660879b4400582f6315fc918777c0fa88185d
ReinhardtJ <ersatz.jonas@yahoo.de>
2020-11-13 22:42:54 +0100

    Added A* prototype

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Transform AiTransform;
+    Transform EnemyTransform;
+    void Start()
+    {
+        AiTransform = GameObject.Find("AI").GetComponent<Transform>();
+        EnemyTransform = gameObject.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        // Bind position of AI to position of enemy
+        var enemyX = EnemyTransform.position.x;
+        var enemyZ = EnemyTransform.position.z;
+        AiTransform.position = new Vector3(enemyX, 0, enemyZ);
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    Rigidbody rigBody;
+    public float moveFactor = 1;
+    void Start()
+    {
+        rigBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Minimal player controls
+        float x = Input.GetAxis("Horizontal") * moveFactor;
+        float z = Input.GetAxis("Vertical") * moveFactor;
+        rigBody.AddForce(new Vector3(x, 0, z));
+    }
+}
