acf1371c3db0265587fa70543b943d490c1c5651
Hexxonite <dominics.pc@gmail.com>
2020-11-15 00:17:25 +0100

    Add ingame UI Icons

--- Assets/Scripts/IngameUI.cs
+++ Assets/Scripts/IngameUI.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class IngameUI : MonoBehaviour
 {
     public GameObject UIText;
+    public GameObject UImgHeavy;
+    public GameObject UImgFeather;
+    public GameObject UImgAlarm;
     public bool Running = true;
     public int decimals = 2;
 
     private TextMeshProUGUI text;
+    private Image rImgHeavy;
+    private Image rImgFeather;
+    private Image rImgAlarm;
     private float score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         text = UIText.GetComponent<TextMeshProUGUI>();
+        Reset();
     }
 
     // Update is called once per frame
@@ -35,8 +43,32 @@ public class IngameUI : MonoBehaviour
         return $"{min:00}:{sec:00},{ms}";
     }
 
+
+    ///////////////////////////////////
+    /// PUBLIC API FOR GAME MANAGER
+    ///////////////////////////////////
+
+
     public void Reset()
     {
         text.SetText(FormatTime(score = 0));
+    }
+
+    // TODO TBD in GameManager?
+    public void SetHeavy(bool visible)
+    {
+        UImgHeavy.SetActive(visible);
+    }
+
+    // TODO TBD in GameManager?
+    public void SetFeather(bool visible)
+    {
+        UImgFeather.SetActive(visible);
+    }
+
+    // TODO TBD in GameManager?
+    public void SetAlarm(bool visible)
+    {
+        UImgAlarm.SetActive(visible);
     }
 }
