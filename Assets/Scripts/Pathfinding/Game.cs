a1bb2bd073be43d6a59a0c8a335982c789ef00d3
ReinhardtJ <ersatz.jonas@yahoo.de>
2020-11-14 12:02:24 +0100

    Added A* Grid recalculation

---
+++ Assets/Scripts/Pathfinding/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    void Start()
+    {
+        InvokeRepeating("UpdateAstarGraph", 2, 2);
+    }
+
+    void Update()
+    {
+    }
+
+    void UpdateAstarGraph() {
+        Debug.LogWarning("Updating Astar Graph");
+        AstarPath.active.Scan();
+        Debug.LogWarning("Updating Astar Graph Done!");
+    }
+}
