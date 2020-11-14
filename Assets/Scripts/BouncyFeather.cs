c81054af1fc4725b748c3575ae6c0e27f323ef18
Hexxonite <dominics.pc@gmail.com>
2020-11-14 16:02:14 +0100

    Add Slowing / Accelerating  Triggers & Control

--- Assets/Scripts/BouncyFeather.cs
+++ Assets/Scripts/BouncyFeather.cs
@@ -1,58 +1,45 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BouncyFeather : MonoBehaviour
 {
-
-    public GameObject[] Targets;
-    public float Bounciness = 2.5f;
-    public float CollisionVolume = 30f;
     // Needs to  be set in-Editor
+    public string CollissionTag = "Player";
     public AudioClip CollisionClip;
 
+    public float Bounciness = 2.5f;
+    public float CollisionVolume = 30f;
 
-    // Representing a runtime audio player
     private AudioSource CollisionAudio;
-
-    // representing "Targets" Rigidbodys for saving performance
-    private Rigidbody[] Bodys;
+    private Renderer Render;
 
 
     // Start is called before the first frame update
     void Start()
     {
         CollisionAudio = gameObject.AddComponent<AudioSource>();
-
-        Bodys = new Rigidbody[Targets.Length];
-        for (int i = 0; i < Targets.Length; i++)
-        {
-            Bodys[i] = Targets[i].GetComponent<Rigidbody>();
-        }
+        Render = gameObject.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Render.material.color = Vector4.Lerp(Render.material.color, Color.white, Time.deltaTime * 0.3f);
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag != CollissionTag) return;
+
+        // play sound
+        float vol = collision.relativeVelocity.magnitude / CollisionVolume;
+        CollisionAudio.PlayOneShot(CollisionClip, Math.Min(1f, Math.Max(0.01f, vol)));
+
+        // change visually
+        Render.material.color = Color.red;
+
+        // reflect off
         Vector3 normal = collision.contacts[0].normal;
-
-        for (int i = 0; i < Targets.Length; i++)
-        {
-            GameObject obj = Targets[i];
-
-            if (collision.gameObject != obj)
-                continue;
-
-            float vol = collision.relativeVelocity.magnitude / CollisionVolume;
-            CollisionAudio.PlayOneShot(CollisionClip, Math.Min(1f, Math.Max(0.01f, vol)));
-
-            Bodys[i].velocity = Vector3.Reflect(collision.relativeVelocity, normal) * Bounciness;
-        }
+        collision.rigidbody.velocity = Vector3.Reflect(collision.relativeVelocity, normal) * Bounciness;
     }
 }
