8b3d52285365bf2f39e7bd5cfd2439ee3e3cabc8
Steven Pauls <stevenpauls@gmx.de>
2020-11-14 12:05:06 +0100

    remote level Demo

---
+++ Assets/Scripts/SyncRemoteObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncRemoteObject : MonoBehaviour
+{
+    public Transform remote_object;
+    public Transform reference_space;
+
+    void LateUpdate()
+    {
+        if (reference_space != null)
+        {
+            remote_object.localPosition = reference_space.worldToLocalMatrix * transform.position;
+            remote_object.localRotation = transform.rotation * reference_space.rotation;
+        }
+        else
+        {
+            remote_object.localPosition = transform.localPosition;
+            remote_object.localRotation = transform.localRotation;
+        }
+    }
+}
