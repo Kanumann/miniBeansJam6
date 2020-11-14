84070eb10052ab8c98bfac018066cee9e2ee1006
Hexxonite <dominics.pc@gmail.com>
2020-11-14 23:39:31 +0100

    Add InGame_UI Prefab, Script & font stuff

---
+++ Assets/Scripts/IngameUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class IngameUI : MonoBehaviour
+{
+    public GameObject UIText;
+    public bool Running = true;
+    public int decimals = 2;
+
+    private TextMeshProUGUI text;
+    private float score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = UIText.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Running) return;
+        score += Time.deltaTime;
+        text.SetText(FormatTime(score));
+    }
+
+    private string FormatTime(float s)
+    {
+        int ms = (int) Math.Round((s - Math.Truncate(s)) * Math.Pow(10, decimals));
+        int sec = (int)s % 60;
+        int min = (int)s / 60;
+        int hrs = (int)min / 60;
+        min = min % 60;
+        return $"{min:00}:{sec:00},{ms}";
+    }
+
+    public void Reset()
+    {
+        text.SetText(FormatTime(score = 0));
+    }
+}
