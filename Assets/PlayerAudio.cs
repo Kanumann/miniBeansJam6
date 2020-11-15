30a304fd2fffa8ef8a3edbf28c5aa5ae2b165a35
Alessandro <me@alessandrofama.com>
2020-11-15 01:18:03 +0100

    Testing Player roll, needs revision

---
+++ Assets/PlayerAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    FMOD.Studio.EventInstance instance;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        TryGetComponent<Rigidbody>(out rb);
+    }
+
+    void Start()
+    {
+        AudioManager.Instance.PostEvent("event:/Player/Ball Rolling", out instance, this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _ = GroundCheck() ? AudioManager.Instance.SetGlobalParameter("g_ballSpeed", rb.velocity.magnitude) 
+                          : AudioManager.Instance.SetGlobalParameter("g_ballSpeed", 0f);
+    }
+
+    bool GroundCheck()
+    {
+        float distance = 0.7f;
+        Vector3 dir = new Vector3(0f, -1f);
+        return _ = Physics.Raycast(transform.position, dir, out RaycastHit hit, distance, 1 << 10) ? true : false;
+    }
+}
