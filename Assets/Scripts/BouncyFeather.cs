57dc065ebbf97ced8d038a8d6475c1a8577c7228
Thomas Eichler <necro1895@gmail.com>
2020-11-15 16:19:29 +0100

    Minimum Prefab Complete

--- Assets/Scripts/BouncyFeather.cs
+++ Assets/Scripts/BouncyFeather.cs
@@ -12,18 +12,16 @@ public class BouncyFeather : MonoBehaviour
     [SerializeField] [FMODUnity.EventRef]
     private string audioEvent = "event:/World/BouncyPlate";
 
-    private Renderer Render;
 
     // Start is called before the first frame update
     void Start()
     {
-        Render = gameObject.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Render.material.color = Vector4.Lerp(Render.material.color, Color.white, Time.deltaTime * 0.3f);
+
     }
 
     void OnCollisionEnter(Collision collision)
@@ -33,9 +31,6 @@ public class BouncyFeather : MonoBehaviour
         // play sound
         float vol = collision.relativeVelocity.magnitude / CollisionVolume;
         AudioManager.Instance.PlayOneShotParameter(audioEvent, "l_volume", vol, this.gameObject);
-
-        // change visually
-        Render.material.color = Color.red;
 
         // reflect off
         Vector3 normal = collision.contacts[0].normal;
