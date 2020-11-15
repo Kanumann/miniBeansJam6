5fbeba7d556c6141e8357e3702341e2102f8448b
ReinhardtJ <ersatz.jonas@yahoo.de>
2020-11-15 13:12:25 +0100

    Added EnemyAI Controller and state for AI Enemies

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    List<GameObject> Enemies;
+
+    // readable sub-lists based on the enemy state
+    List<GameObject> ChasingPlayerEnemies {
+        get { return Enemies.FindAll(enemy => enemy.GetComponent<EnemyAI>().enemyState == EnemyState.CHASING_PLAYER); }
+    }
+    List<GameObject> EngagingMapPositionEnemies {
+        get { return Enemies.FindAll(enemy => enemy.GetComponent<EnemyAI>().enemyState == EnemyState.ENGAGING_MAP_TARGET); }
+    }
+    List<GameObject> WaitingEnemies {
+        get { return Enemies.FindAll(enemy => enemy.GetComponent<EnemyAI>().enemyState == EnemyState.WAITING); }
+    }
+    List<GameObject> NonChasingPlayerEnemies {
+        get { return Enemies.FindAll(enemy => enemy.GetComponent<EnemyAI>().enemyState != EnemyState.CHASING_PLAYER); }
+    }
+    List<GameObject> NonEngagingMapPositionEnemies {
+        get { return Enemies.FindAll(enemy => enemy.GetComponent<EnemyAI>().enemyState != EnemyState.ENGAGING_MAP_TARGET); }
+    }
+    List<GameObject> NonWaitingEnemies {
+        get { return Enemies.FindAll(enemy => enemy.GetComponent<EnemyAI>().enemyState != EnemyState.WAITING); }
+    }
+
+    void Start()
+    {
+        Enemies = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void addEnemy(GameObject enemy) {
+        Enemies.Add(enemy);
+    }
+
+    public void startChasingPlayer(int numberOfEnemies = 1) {
+        for(int i = 0; i<numberOfEnemies; i++) {
+            if (NonChasingPlayerEnemies.Count > 0) {
+                NonChasingPlayerEnemies[0].GetComponent<EnemyAI>()
+                                        .changeState(EnemyState.CHASING_PLAYER);
+            }
+        }
+    }
+
+    public void startEngagingRandomMapTarget(int numberOfEnemies = 1) {
+        for(int i = 0; i<numberOfEnemies; i++) {
+            if (NonEngagingMapPositionEnemies.Count > 0) {
+                NonEngagingMapPositionEnemies[0].GetComponent<EnemyAI>()
+                                                .changeState(EnemyState.ENGAGING_MAP_TARGET); 
+            }
+        }
+    }
+
+    public void startWatiting(int numberOfEnemies = 1) {
+        for(int i = 0; i<numberOfEnemies; i++) {
+            if (NonWaitingEnemies.Count > 0) {
+                NonWaitingEnemies[0].GetComponent<EnemyAI>()
+                                    .changeState(EnemyState.WAITING);
+            }
+        }
+    }
+}
--- Assets/Scripts/Pathfinding/EnemyAI.cs
+++ Assets/Scripts/Pathfinding/EnemyAI.cs
@@ -2,33 +2,42 @@
 using UnityEngine.AI;
 
 
-// @TODO public schnittstelle zum Spawnen von gegnern mit position
+public enum EnemyState {
+    WAITING, CHASING_PLAYER, ENGAGING_MAP_TARGET
+}
 
 public class EnemyAI : MonoBehaviour
 {
+    // TODO(Jonas): Overwrite maxDistanceToHit depending on player and enemy size
     public float maxDistanceToHit = 1.1f;
     public float hitStrength = 20f;
-    public float inactiveTime = 2f;
+    public float howLongToWait = 2f;
     public float checkForRespawnInterval = 2f;
     public float predictVelocity = 0.1f;
+    public EnemyState enemyState { get; private set; } 
+
+    // When enemy is engaging a new map target, pick a position to go to within this radius 
+    public float walkRadius = 10f;
+    
+    // on first run, wait longer
+    public float remainingWaitSeconds = 3f; 
 
     private Vector3 OwnPosition { 
         get { return gameObject.transform.position; }
     }
 
+    // When chasing player
     private GameObject localTarget;
-    private Vector3 TargetPosition {
+    private Vector3 PlayerTargetPosition {
          get { return localTarget.transform.position; } 
     }
+    // When not chasing player
+    private Vector3 MapTargetPosition;
 
 
-    // TODO(Jonas): Overwrite maxDistanceToHit depending on player 
-    //            :and enemy size
     private Rigidbody remoteRigid;
     private Vector3 initialPosition;
     private NavMeshAgent navAgent;
-
-    private float waitSeconds = 3f; // on first run, wait longer
 
     void Start() {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
@@ -44,32 +53,69 @@ public class EnemyAI : MonoBehaviour
                         this.checkForRespawnInterval);
     }
 
-    void Update() {
-        // wait for Hit cooldown
-        if (waitSeconds > 0)
-        {
-            waitSeconds -= Time.deltaTime;
-            return;
+    public void changeState(EnemyState state) {
+        switch (state) {
+            case EnemyState.CHASING_PLAYER: 
+                break;
+            case EnemyState.ENGAGING_MAP_TARGET:
+                MapTargetPosition = getRandomPositionOnNavMesh(); 
+                break;
+            case EnemyState.WAITING:
+                remainingWaitSeconds += howLongToWait; 
+                break;
         }
+        this.enemyState = state;
+    }
 
-        // tell agent to continue navigating
-        navAgent.destination = TargetPosition + remoteRigid.velocity * predictVelocity;
-
-        // Hit target if close enough
-        if (DistanceToPlayer() <= maxDistanceToHit)
-        {
-            HitPlayer();
-            waitSeconds = inactiveTime;
+    void Update() {
+        switch (this.enemyState) {
+            case EnemyState.CHASING_PLAYER: 
+                chasePlayer();
+                break;
+            case EnemyState.ENGAGING_MAP_TARGET:
+                engageMapTarget();
+                break;
+            case EnemyState.WAITING:
+                return;
         }
     }
 
+    private void wait() {
+        // wait for Hit cooldown
+        if (remainingWaitSeconds > 0) {
+            remainingWaitSeconds -= Time.deltaTime;
+        } else {
+            // TODO(Jonas): chase player or go to random location based on what condition?
+            changeState(EnemyState.CHASING_PLAYER);
+        }
+    }
+
+    private void chasePlayer() {
+        // tell agent to continue navigating
+        navAgent.destination = PlayerTargetPosition + remoteRigid.velocity * predictVelocity;
+
+        // Hit target if close enough
+        if (DistanceToPlayer() <= maxDistanceToHit) {
+            HitPlayer();
+            remainingWaitSeconds = howLongToWait;
+            changeState(EnemyState.WAITING);
+        }
+    }
+
+    private void engageMapTarget() {
+        navAgent.destination = MapTargetPosition;
+        // start wating when map target is reached
+        if (navAgent.pathStatus == NavMeshPathStatus.PathComplete)
+            changeState(EnemyState.WAITING);
+    }
+
     private float DistanceToPlayer() {
-        return Vector3.Distance(TargetPosition, OwnPosition);
+        return Vector3.Distance(PlayerTargetPosition, OwnPosition);
     }
 
 
     private void HitPlayer() {
-        var directionToPlayer = TargetPosition - OwnPosition;
+        var directionToPlayer = PlayerTargetPosition - OwnPosition;
         directionToPlayer.y = 1f;
         directionToPlayer.Normalize();
         remoteRigid.AddForce(directionToPlayer * hitStrength, ForceMode.Impulse);
@@ -80,5 +126,16 @@ public class EnemyAI : MonoBehaviour
         if (y < -20f) {
             gameObject.transform.position = initialPosition;
         }
+    }
+
+    private Vector3 getRandomPositionOnNavMesh() {
+        Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1)) {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
     }
 }
