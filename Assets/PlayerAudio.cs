9fff3a569809405af205234870f1f240627aa9a6
Alessandro <me@alessandrofama.com>
2020-11-15 02:00:59 +0100

    Triggers audio

--- Assets/PlayerAudio.cs
+++ Assets/PlayerAudio.cs
@@ -31,4 +31,10 @@ public class PlayerAudio : MonoBehaviour
         Vector3 dir = new Vector3(0f, -1f);
         return _ = Physics.Raycast(transform.position, dir, out RaycastHit hit, distance, 1 << 10) ? true : false;
     }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Slowing") AudioManager.Instance.PlayOneShot("event:/World/Trigger1", this.gameObject);
+        if (collider.tag == "Accelerating") AudioManager.Instance.PlayOneShot("event:/World/Trigger2", this.gameObject);
+    }
 }
